dc0121c6f09aa5138be9b55222ffd49c603ce84a
mannujr <116248896+mannujr@users.noreply.github.com>
2025-03-06 07:18:11 +0530

    Fixed duplicate key issue and missing asset

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -5,9 +5,9 @@ export const Marquee = () => {
   return (
     <div data-scroll data-scroll-section data-scroll-speed=".1" className='w-full bg-[#004d43] py-10 '>
        <div className='border-t-2 font-extrabold border-b-2 border-zinc-100 flex whitespace-nowrap  overflow-hidden'>
-        <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{repeat:"infinity",ease:"linear", repeat: Infinity, duration:14}} className='text-[12vw] leading-none'>NOT JUST A ORDINARY DESIGNER!</motion.h1>
-        <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{repeat:"infinity",ease:"linear", repeat: Infinity, duration:14}} className='text-[12vw] leading-none'>NOT JUST A ORDINARY DESIGNER!</motion.h1>
-        <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{repeat:"infinity",ease:"linear", repeat: Infinity, duration:14}} className='text-[12vw] leading-none'>NOT JUST A ORDINARY DESIGNER!</motion.h1>
+        <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear", repeat: Infinity, duration:14}} className='text-[12vw] leading-none'>NOT JUST A ORDINARY DESIGNER!</motion.h1>
+        <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear", repeat: Infinity, duration:14}} className='text-[12vw] leading-none'>NOT JUST A ORDINARY DESIGNER!</motion.h1>
+        <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear", repeat: Infinity, duration:14}} className='text-[12vw] leading-none'>NOT JUST A ORDINARY DESIGNER!</motion.h1>
        </div>
     </div>
   )
